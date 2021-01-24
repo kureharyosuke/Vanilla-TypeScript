2700d7e494e4760370a9628dbb16ca0c327d05cf
kureharyosuke <ohhayo.com@gmail.com>
2021-01-24 18:43:00 +0900

    #15. Generic - Union Type

--- class-note/8_generice.ts
+++ class-note/8_generice.ts
@@ -22,3 +22,28 @@ logText<boolean>(true) // function logText<boolean>(text: boolean): boolean
 logText<number[]>([]); // function logText<number[]>(text: number[]): number[]
 logText<string[]>([]); // function logText<string[]>(text: string[]): string[]
 logText<object[]>([]); // function logText<object[]>(text: object[]): object[]
+
+function logTexts(text:string | number) {
+    console.log(text)
+    text.split('').reverse().join(''); 
+    // Property 'split' does not exist on type 'string | number'.
+    // Property 'split' does not exist on type 'number'.
+    return text
+}
+
+function logNumbers(num: number) {
+    console.log(num);
+    return num;
+}
+
+function logInterSection(interSection: string & number) {
+    console.log(interSection);
+    return interSection;
+}
+
+logTexts('a');
+logTexts(10); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+// Union Type (text:string | number) not error
+
+logNumbers(10); // function logNumbers(num: number): number
+const num = logNumbers(10); // const num: number

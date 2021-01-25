1c2865e38eadbbdba979b51f105f949c363dc795
kureharyosuke <ohhayo.com@gmail.com>
2021-01-25 14:32:47 +0900

    #16-1. Dropdown - interface version

--- class-note/dropdown-generic.ts
+++ class-note/dropdown-generic.ts
@@ -1,30 +1,77 @@
 /**
- *  
+ *
  */
 
-const emails = [
-    { value: 'google.com', selected: true},
-    { value: 'yahoo.co.jp', selected: false},
-    { value: 'naver.com', selected: false}
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+// const emails: { value: string; selected: boolean }[] = [
+//   { value: "google.com", selected: true },
+//   { value: "yahoo.co.jp", selected: false },
+//   { value: "naver.com", selected: false },
+// ];
+
+const emails: Email[] = [
+  { value: "google.com", selected: true },
+  { value: "yahoo.co.jp", selected: false },
+  { value: "naver.com", selected: false },
 ];
 
-const numberOfProducts = [
-    { value: 1, selected: true},
-    { value: 2, selected: false},
-    { value: 3, selected: false},
-]
+interface ProductNumber {
+  value: number;
+  selected: boolean;
+}
 
-function createDropdownItem(item: { value: string; selected: boolean}){
-    const option = document.createElement('option');
-    option.value = item.value.toString();
-    option.innerText = item.value.toString();
-    option.selected = item.selected;
-    return option
+interface TrueFalse {
+  value: boolean;
+  selected: boolean;
+}
+
+// const numberOfProducts: { value: number; selected: boolean }[] = [
+//   { value: 1, selected: true },
+//   { value: 2, selected: false },
+//   { value: 3, selected: false },
+// ];
+
+const numberOfProducts: ProductNumber[] = [
+  { value: 1, selected: true },
+  { value: 2, selected: false },
+  { value: 3, selected: false },
+];
+
+// function createDropdownItem(
+//   item:
+//     | { value: number; selected: boolean }
+//     | { value: string; selected: boolean }
+// ) {
+//   const option = document.createElement("option");
+//   option.value = item.value.toString();
+//   option.innerText = item.value.toString();
+//   option.selected = item.selected;
+//   return option;
+// }
+
+function createDropdownItem(
+  item: Email | ProductNumber
+  //   | { value: number; selected: boolean }
+  //   | { value: string; selected: boolean }
+) {
+  const option = document.createElement("option");
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-    const item = createDropdownItem(email);
-    const selectTag = document.querySelector('#email-dropdown')
-    selectTag.appendChild(item);
-})
+  const item = createDropdownItem(email);
+  const selectTag = document.querySelector("#email-dropdown");
+  selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product);
+});

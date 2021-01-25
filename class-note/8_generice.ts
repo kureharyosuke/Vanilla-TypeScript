72b9b8943fb4fb38dd39bdcf5205134387f13a08
kureharyosuke <ohhayo.com@gmail.com>
2021-01-25 14:40:04 +0900

    #16-2. Dropdown - interface Generic

--- class-note/8_generice.ts
+++ class-note/8_generice.ts
@@ -9,44 +9,44 @@
 // logText('Hi');
 // logText(true);
 
-function logText<T>(text: T):T {
-    console.log(text);
-    return text;
+function logText<T>(text: T): T {
+  console.log(text);
+  return text;
 }
 
-logText('Hi');         // function logText<"Hi">(text: "Hi"): "Hi"
-logText<string>('Hi'); // function logText<string>(text: string): string
-logText<number>(1);    // function logText<number>(text: number): number
-logText<object>({});   // function logText<object>(text: object): object
-logText<boolean>(true) // function logText<boolean>(text: boolean): boolean
+logText("Hi"); // function logText<"Hi">(text: "Hi"): "Hi"
+logText<string>("Hi"); // function logText<string>(text: string): string
+logText<number>(1); // function logText<number>(text: number): number
+logText<object>({}); // function logText<object>(text: object): object
+logText<boolean>(true); // function logText<boolean>(text: boolean): boolean
 logText<number[]>([]); // function logText<number[]>(text: number[]): number[]
 logText<string[]>([]); // function logText<string[]>(text: string[]): string[]
 logText<object[]>([]); // function logText<object[]>(text: object[]): object[]
 
-function logTexts(text:string | number) {
-    console.log(text)
-    // text.
-    // text.split('').reverse().join(''); // Error is Union Type number
-    // Property 'split' does not exist on type 'string | number'.
-    // Property 'split' does not exist on type 'number'.
-    return text
+function logTexts(text: string | number) {
+  console.log(text);
+  // text.
+  // text.split('').reverse().join(''); // Error is Union Type number
+  // Property 'split' does not exist on type 'string | number'.
+  // Property 'split' does not exist on type 'number'.
+  return text;
 }
 
 function logNumbers(num: number) {
-    console.log(num);
-    return num;
+  console.log(num);
+  return num;
 }
 
 function logInterSection(interSection: string & number) {
-    //function logInterSection(interSection: string & number): never
-    console.log(interSection);
-    return interSection;
+  //function logInterSection(interSection: string & number): never
+  console.log(interSection);
+  return interSection;
 }
 
-const b = logTexts('a')
+const b = logTexts("a");
 // a.split();
 
-logTexts('a');
+logTexts("a");
 logTexts(10); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
 // Union Type (text:string | number) not error
 
@@ -55,16 +55,31 @@ const num = logNumbers(10); // const num: number
 
 /**
  *  제너릭에 대한 사용법
- * @param text 
+ * @param text
  */
 
 function logTextA<T>(text: T): T {
-    console.log(text);
-    return text;
+  console.log(text);
+  return text;
 }
 
-const str = logTextA<string>('abc');
-str.split(''); // (method) String.split(separator: string | RegExp, limit?: number): string[] (+1 overload)
+const str = logTextA<string>("abc");
+str.split(""); // (method) String.split(separator: string | RegExp, limit?: number): string[] (+1 overload)
 
 const login = logTextA<boolean>(true);
 login.valueOf(); //(method) Boolean.valueOf(): boolean
+
+// 인터페이스에 제네릭을 선언하는 방법
+interface Dropdown {
+  value: string;
+  selected: boolean;
+}
+
+const obj: Dropdown = { value: "abc", selected: false };
+
+interface Dropdown2<T> {
+  value: T;
+  selected: boolean;
+}
+
+const obj2: Dropdown2<string> = { value: "abc", selected: false };

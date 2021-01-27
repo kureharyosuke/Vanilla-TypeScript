5142085b428b2429e3901a07c95d46c90896f18c
kureharyosuke <ohhayo.com@gmail.com>
2021-01-28 00:11:04 +0900

    #18-1 Best Common Type

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -43,3 +43,30 @@ var detailedItem: DetailDropdown<string> = {
     tag: 'a', //(property) DetailDropdown<string>.tag: string
 }
 
+// Best Common Type
+
+var arr = [1, 2, true]; // var arr: (number | boolean)[]
+var arr2 = [1,2,3, true, 'a'] //var arr2: (string | number | boolean)[]
+
+// Contextual Typing
+
+window.onmousedown = function(mouseEvent) {
+    console.log(mouseEvent.button);   //<- OK
+    // console.log(mouseEvent.kangaroo); //<- Error!
+  };
+
+window.onscroll = function(uiEvent) {
+    console.log(uiEvent.button); //<- Error!
+  }
+
+  const handler = function(uiEvent) {
+    console.log(uiEvent.button); //<- OK
+  }
+
+/**
+ * 
+ * TIP
+ * Duck Typing : 객체의 변수 및 메서드의 집합이 객체의 타입을 결정하는 것을 의미. 
+ *               동적 타이핑의 한 종류 
+ * Structural Subtyping : 객체의 실제 구조나 정의에 따라 타입을 결정하는 것을 의미
+ */

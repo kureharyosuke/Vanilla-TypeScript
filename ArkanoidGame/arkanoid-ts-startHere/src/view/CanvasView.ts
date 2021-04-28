571a0b612fb25f11b1961296c57b3786b61d7271
kureharyosuke <ohhayo.com@gmail.com>
2021-04-28 18:31:50 +0900

    add: Canvas View

--- ArkanoidGame/arkanoid-ts-startHere/src/view/CanvasView.ts
+++ ArkanoidGame/arkanoid-ts-startHere/src/view/CanvasView.ts
@@ -17,4 +17,36 @@ export class CanvasView {
     this.start = document.querySelector("#start");
     this.info = document.querySelector("#info");
   }
+
+  clear(): void {
+    this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  initStartButton(startFunction: (view: CanvasView) => void): void {
+    this.start?.addEventListener("click", () => startFunction(this));
+  }
+
+  drawScore(score: number): void {
+    if (this.scoreDisplay) this.scoreDisplay.innerHTML = score.toString();
+  }
+
+  drawInfo(text: string): void {
+    if (this.info) this.info.innerHTML = text;
+  }
+
+  drawSprite(brick: Brick | Paddle | Ball): void {
+    if (!brick) return;
+
+    this.context?.drawImage(
+      brick.image,
+      brick.pos.x,
+      brick.pos.y,
+      brick.width,
+      brick.height
+    );
+  }
+
+  drawBricks(brick: Brick[]): void {
+    brick.forEach((Brick) => this.drawSprite(brick));
+  }
 }

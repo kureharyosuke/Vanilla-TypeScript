a02cb16ba6c6e136d8ae46aac52e9dc1a8b5741b
kureharyosuke <ohhayo.com@gmail.com>
2021-06-13 19:15:19 +0900

    add: loader

--- DesignPatterns/src/index.ts
+++ DesignPatterns/src/index.ts
@@ -1,7 +1,12 @@
-import {} from "./loader";
+import { loader, RecordHandler } from "./loader";
 
 interface Pokemon {
   id: string;
   attack: number;
   defense: number;
+}
+
+interface Database<T> {
+  set(newValue: T): void;
+  get(id: string): T;
 }

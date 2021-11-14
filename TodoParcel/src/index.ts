24a9ff1beaf3252c195dbf91f03d34120f83aa80
kureharyosuke <ohhayo.com@gmail.com>
2021-11-14 19:23:50 +0900

    add: container.innerHTML todo form submit

--- TodoParcel/src/index.ts
+++ TodoParcel/src/index.ts
@@ -4,5 +4,29 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function todo(container: Element) {
-  container.innerHTML = `<div></div>`;
+  container.innerHTML = `
+  <h2>Todo</h2>
+  <ul></ul>
+  <h3>0 Done</h3>
+  <form>
+    <input type="text" name="text" />
+    <button type="sumbit">Add</button>
+  </form>
+  `;
+
+  const list = container.querySelector<HTMLUListElement>("ul");
+  const done = container.querySelector<HTMLHeadingElement>("h3");
+  const form = container.querySelector<HTMLFormElement>("form");
+
+  form.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+
+    // form in input
+    const input = form.elements["text"] as HTMLInputElement;
+
+    // form reset
+    form.reset();
+    // form focus
+    form.focus();
+  });
 }
